0f0fef8a60284d106bd776d9e8f5aca0055202c6
dianaguerrero <dguerreropacheco@gmail.com>
2026-06-01 00:10:18 -0500

    v2.0

--- sistema_horarios/settings.py
+++ sistema_horarios/settings.py
@@ -28,8 +28,15 @@ SECRET_KEY = 'django-insecure-r8ruid83-6kmf2x5qy16wd8n%(dtvptv1=i578z^nl6iyg&^qb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'proyectodjangosistemahorario-production.up.railway.app',
+    'localhost',
+    '127.0.0.1'
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://railway.app'
+]
 
 # Application definition
 
